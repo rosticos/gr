49922fc87516e5b5ed0ab93000859c1b531bf067
rskabali <rskabali@mts.ru>
2021-05-13 17:37:16 +0300

    update files

--- src/components/plotly/methods.js
+++
@@ -1,8 +0,0 @@
-const plotlyFunctions = ["restyle", "relayout", "update", "addTraces", "deleteTraces", "moveTraces", "extendTraces", "prependTraces", "purge"];
-
-export const methods = plotlyFunctions.reduce((all, functionName) => {
-  all[functionName] = function(...args) {
-    return Plotly[functionName].apply(Plotly, [this.$el, ...args]);
-  };
-  return all;
-}, {});
