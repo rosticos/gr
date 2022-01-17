b8ae2fd691ccaa58c148f95f1359ca3eefb74fa8
Ростислав Кабалин <rskabali@mts.ru>
2022-01-17 12:03:15 +0300

    feat/add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directorycd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:rosticos/gr.git master:gh-pages
+cd -
